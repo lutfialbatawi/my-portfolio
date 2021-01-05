1baccd75d55835ef213b31afff333bb393e54ddc
Lutfi_albatawi <lutfialbatawi@gmail.com>
2021-01-05 10:50:29 +0700

    portfolio

---
+++ jss/index-1.js
@@ -0,0 +1,12 @@
+alert('Selamat Datang');
+var lagi = true;
+
+while ( lagi === true ) {
+    var nama=prompt('Siapa Nama Anda');
+    alert(' Hy '+ nama);
+
+    // tanya Lagi
+    lagi = confirm('Apakah Anda Ingin Mencoba lagi ?');
+}
+// CLOSING
+alert(' Terima Kasih ' + nama);
